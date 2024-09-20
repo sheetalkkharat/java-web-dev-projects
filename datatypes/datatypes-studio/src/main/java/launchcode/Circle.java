a214ccd5d6af8ac6b607de787fcd3450fcf607dd
sheetalkkharat <sheetalkharatk@gmail.com>
2024-09-19 20:35:58 -0500

    Created 2 files

--- datatypes/datatypes-studio/src/main/java/launchcode/Circle.java
+++
@@ -1,4 +0,0 @@
-package launchcode;
-
-public class Circle {
-}
---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+        double radius;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+        System.out.println("Hey!");
+    }
+
+}
